4673feac8409231d814e4f3681858bc61e0c2fb1
Ashish Pandey <ashish.jnvm@gmail.com>
2024-02-17 21:19:37 +0000

    adding db setup

--- db/db_setup.py
+++ db/db_setup.py
@@ -1,13 +1,29 @@
-from sqlalchemy import create_engine, true
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from .config import (
+    DB_HOST as host,
+    DB_PORT as port,
+    DB_USERNAME as uname,
+    DB_PASSWORD as passwd,
+    DB_NAME as db,
+)
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{uname}:{passwd}@{host}:{port}/{db}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     future=True,
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from db.db_setup import get_db
 
 app = FastAPI()
 
